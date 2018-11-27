66ec885087e50e784560962e7ac7e3ba6d2d8478
sylvain <sthibault63@gmail.com>
2018-11-27 14:20:30 +0100

    Ha oui au fait

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Beginner\'s Tutorial';
+  subtitle = 'Tour of Heroes !!';
+}
