b872be3cff2fabf78ebf6ab5b4659e43e12868ef
sylvain <sthibault63@gmail.com>
2018-11-27 14:01:35 +0100

    Création de la classe 'hero' et affichage

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
